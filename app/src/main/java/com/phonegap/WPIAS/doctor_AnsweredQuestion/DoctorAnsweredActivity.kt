09e27bea7cdda4f75b8e02cc3d60c4901f485446
min92822 <jeong920822@gmail.com>
2020-01-13 16:47:12 +0900

    V 28 for test

--- app/src/main/java/com/phonegap/WPIAS/doctor_AnsweredQuestion/DoctorAnsweredActivity.kt
+++ app/src/main/java/com/phonegap/WPIAS/doctor_AnsweredQuestion/DoctorAnsweredActivity.kt
@@ -159,11 +159,9 @@ class DoctorAnsweredActivity : RootActivity() {
     //서버 통신 완료되기까지 view visible을 컨트롤 한다
     fun viewVisibleControl(result : Boolean){
         if(result) {
-            constraintLayout11.visibility = View.VISIBLE
             questionRecyclerView.visibility = View.VISIBLE
             Loading(ProgressBar, ProgressBg, !result)
         }else{
-            constraintLayout11.visibility = View.GONE
             questionRecyclerView.visibility = View.GONE
             Loading(ProgressBar, ProgressBg, !result)
         }
